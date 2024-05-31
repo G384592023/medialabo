ca7cbafffc81e61fe741a77ba65ac11caf698d94
G384592023 <238459@st.takushoku-u.ac.jp>
2024-05-31 22:10:54 +0900

    天気js

--- weather.js
+++ weather.js
@@ -46,80 +46,113 @@ let data = {
 };
 
 ////////// 課題3-2 ここからプログラムを書こう
+
+let dq = document.querySelector('div#first')
+let p = document.createElement('p');
+p.textContent = '世界の天気(検索結果2件)';
+dq.insertAdjacentElement('beforeend', p);
+
 let b1 = document.querySelector('button#btn');
 b1.addEventListener('click', showSelectResult);
-
+let key = 0;
 function showSelectResult() {
-  let s = document.querySelector('select#toshi');
+  let s = document.querySelector('select#name');
   let idx = s.selectedIndex;  // idx 番目の option が選択された
 
   let os = s.querySelectorAll('option');  // s の子要素 option をすべて検索
   let o = os.item(idx);       // os の idx 番目の要素
-
   console.log('検索キー:' + o.getAttribute('value'));  // id 属性を表示
+  key = Number(o.getAttribute('value'));
+
   let cs = document.querySelectorAll('input[name="tenki"]');
-	  for (let c of cs) {
-		  if (c.checked) {
-			  console.log('ジャンル:' + c.value);
-		  }
-	  }
+  let checkedGenres = [];
+  for (let c of cs) {
+    if (c.checked) {
+      checkedGenres.push(c.value);
+      console.log('ジャンル:' + c.value);
+
+    }
+  }
+
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + key + '.json';
+  axios.get(url)
+    .then(showResult)
+    .catch(showError)
+    .then(finish);
 }
+let na;
+function showResult(resp) {
+  let data = resp.data;
+  if (typeof data === 'string') {
+      data = JSON.parse(data);
+  }
+  na=data.name;
+  console.log(data.name);
+  // data をコンソールに出力
+  processData(data);
+}
+function processData(data) {
+  let w = document.querySelectorAll('li')
+for(let n of w){
+	n.remove();
+}
+  let dp = document.querySelector('div#result')
+  let u1 = document.createElement('ul');
+  dp.appendChild(u1);
 
+  let p1 = document.createElement('li');
+  p1.textContent = '検索結果';
+  let u2 = document.createElement('ul');
+  u1.insertAdjacentElement('beforeend', u2);
+  u2.insertAdjacentElement('beforeend', p1); 
 
-let dp = document.querySelector('div#result')
+  let p2 = document.createElement('li');
+  p2.textContent = ('都市名:' + data.name);
+  u2.insertAdjacentElement('beforeend', p2); 
 
-let p = document.createElement('li');
-p.textContent = '世界の天気(検索結果2件)';
-let u = document.createElement('ul');
-u.insertAdjacentElement('beforeend', p);
-dp.insertAdjacentElement('beforeend', u);
-
-let p1 = document.createElement('li');
-p1.textContent = '検索結果1件目';
-let u1 = document.createElement('ul');
-u.insertAdjacentElement('beforeend', u1);
-u1.insertAdjacentElement('beforeend', p1);
-
-
-let p2 = document.createElement('li');
-p2.textContent = ('都市名:' + data.name);
-let u2 = document.createElement('ul');
-u.insertAdjacentElement('beforeend', u1);
-u1.insertAdjacentElement('beforeend', u2);
-u2.insertAdjacentElement('beforeend', p2); 
-
-let p3 = document.createElement('li');
-p3.textContent = ('天気:' + data.weather[0].description);
-u.insertAdjacentElement('beforeend', u1);
-u1.insertAdjacentElement('beforeend', u2);
-u2.insertAdjacentElement('beforeend', p3); 
-
-let p4 = document.createElement('li');
-p4.textContent = ('最高気温:' + data.main.temp_max);
-u.insertAdjacentElement('beforeend', u1);
-u1.insertAdjacentElement('beforeend', u2);
-u2.insertAdjacentElement('beforeend', p4);
-
-let p5 = document.createElement('li');
-p5.textContent = ('最低気温:' + data.main.temp_min);
-u.insertAdjacentElement('beforeend', u1);
-u1.insertAdjacentElement('beforeend', u2);
-u2.insertAdjacentElement('beforeend', p5);
-
-let p6 = document.createElement('li');
-p6.textContent = ('湿度:' + data.main.humidity);
-u.insertAdjacentElement('beforeend', u1);
-u1.insertAdjacentElement('beforeend', u2);
-u2.insertAdjacentElement('beforeend', p6);
-
-let p7 = document.createElement('li');
-p7.textContent = ('風速:' + data.wind.speed);
-u.insertAdjacentElement('beforeend', u1);
-u1.insertAdjacentElement('beforeend', u2);
-u2.insertAdjacentElement('beforeend', p7);
-
-let p8 = document.createElement('li');
-p8.textContent = ('風向:' + data.wind.deg);
-u.insertAdjacentElement('beforeend', u1);
-u1.insertAdjacentElement('beforeend', u2);
-u2.insertAdjacentElement('beforeend', p8);
+  let cs = document.querySelectorAll('input[name="tenki"]');
+  for (let genre of cs) {
+    if (genre.checked) {
+      switch (genre.value) {
+        case 'weather[0].description':
+          let p3 = document.createElement('li');
+          p3.textContent = ('天気:' + data.weather[0].description);
+          u2.insertAdjacentElement('beforeend', p3); 
+          break;
+        case 'main.temp_max':
+          let p4 = document.createElement('li');
+          p4.textContent = ('最高気温:' + data.main.temp_max);
+          u2.insertAdjacentElement('beforeend', p4);
+          break;
+        case 'main.temp_min':
+          let p5 = document.createElement('li');
+          p5.textContent = ('最低気温:' + data.main.temp_min);
+          u2.insertAdjacentElement('beforeend', p5);
+          break;
+        case 'main.humidity':
+          let p6 = document.createElement('li');
+          p6.textContent = ('湿度:' + data.main.humidity);
+          u2.insertAdjacentElement('beforeend', p6);
+          break;
+        case 'wind.speed':
+          let p7 = document.createElement('li');
+          p7.textContent = ('風速:' + data.wind.speed);
+          u2.insertAdjacentElement('beforeend', p7);
+          break;
+        case 'wind.deg':
+          let p8 = document.createElement('li');
+          p8.textContent = ('風向:' + data.wind.deg);
+          u2.insertAdjacentElement('beforeend', p8);
+          break;
+        default:
+          console.log('選択されたジャンルに対応するデータがありません');
+      }
+    }
+  }
+}
+function showError(err) {
+  console.log(err);
+}
+function finish() {
+  console.log('Ajax 通信が終わりました');
+}
